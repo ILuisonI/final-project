1a3eab791f635d5f560e65fcd97362f2182e8386
Fernas <firnasb@hotmail.com>
2023-08-17 11:49:59 +0300

    Final

--- model/mongodb/plants/helpers/normalizationPlant.js
+++ model/mongodb/plants/helpers/normalizationPlant.js
@@ -2,12 +2,10 @@ const normalizePlant = async (plant, userId) => {
   if (!plant.imageUrl) {
     plant.imageUrl = null;
   }
-  plant.image = {
-    imageUrl:
-      plant.imageUrl ||
-      "https://media.istockphoto.com/id/1367401626/vector/set-silhouette-houseplant-indoor-black-and-white-house-plants-in-flower-pot-outline-doodle.jpg?s=612x612&w=0&k=20&c=gK5r92EcNkMfQKTbsm3Hqx6Cw0OYGf8-fAuRzkFCmBs=",
-    imageAlt: plant.imageAlt || "Plant",
-  };
+  plant.imageUrl =
+    plant.imageUrl ||
+    "https://media.istockphoto.com/id/1367401626/vector/set-silhouette-houseplant-indoor-black-and-white-house-plants-in-flower-pot-outline-doodle.jpg?s=612x612&w=0&k=20&c=gK5r92EcNkMfQKTbsm3Hqx6Cw0OYGf8-fAuRzkFCmBs=";
+  plant.imageAlt = plant.imageAlt || "Plant";
   return {
     ...plant,
     user_id: plant.user_id || userId,
