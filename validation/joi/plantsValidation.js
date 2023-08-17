6fd1d941147780bb5eebd98c440bd7bb8377240b
Fernas <firnasb@hotmail.com>
2023-08-17 14:44:05 +0300

    Final

--- validation/joi/plantsValidation.js
+++ validation/joi/plantsValidation.js
@@ -12,7 +12,7 @@ const createPlantSchema = Joi.object({
       new RegExp(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)
     )
     .required(),
-  price: Joi.number().min(0).max(256).required(),
+  price: Joi.number().min(1).required(),
   imageUrl: Joi.string().regex(
     new RegExp(
       /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/
