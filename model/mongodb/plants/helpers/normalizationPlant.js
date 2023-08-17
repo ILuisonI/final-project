53362513f1c2572cc52e523ae073c929103b713e
Fernas <firnasb@hotmail.com>
2023-08-17 11:47:16 +0300

    Final

--- model/mongodb/plants/helpers/normalizationPlant.js
+++ model/mongodb/plants/helpers/normalizationPlant.js
@@ -1,19 +1,15 @@
 const normalizePlant = async (plant, userId) => {
-  if (!plant.image) {
-    plant.image = {};
+  if (!plant.imageUrl) {
+    plant.imageUrl = null;
   }
   plant.image = {
-    url:
-      plant.image.url ||
+    imageUrl:
+      plant.imageUrl ||
       "https://media.istockphoto.com/id/1367401626/vector/set-silhouette-houseplant-indoor-black-and-white-house-plants-in-flower-pot-outline-doodle.jpg?s=612x612&w=0&k=20&c=gK5r92EcNkMfQKTbsm3Hqx6Cw0OYGf8-fAuRzkFCmBs=",
-    alt: plant.image.alt || "Plant",
+    imageAlt: plant.imageAlt || "Plant",
   };
   return {
     ...plant,
-    address: {
-      ...plant.address,
-      state: plant.address.state || "",
-    },
     user_id: plant.user_id || userId,
   };
 };
